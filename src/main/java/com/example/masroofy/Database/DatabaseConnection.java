cebbabc5e064b5c1eaef8da0e314788042d1d89f
ELOSTAKIMO <20240424@stud.fci-cu.edu.eg>
2026-04-28 01:08:43 +0300

    Database Schema Added

---
+++ src/main/java/com/example/masroofy/Database/DatabaseConnection.java
@@ -0,0 +1,5 @@
+package com.example.masroofy.Database;
+
+public class DatabaseConnection {
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,8 @@ module com.example.masroofy {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
 
+    opens com.example.masroofy to javafx.fxml;
+
     opens com.example.masroofy.View to javafx.fxml;
     exports com.example.masroofy;
 }
