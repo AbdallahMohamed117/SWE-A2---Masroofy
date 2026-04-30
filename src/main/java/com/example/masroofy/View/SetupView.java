621f059a897eb679ac2b66d92955ec9216cff2eb
sama hany <20240250@stud.fci-cu.edu.eg>
2026-04-30 13:46:38 +0300

    The design was changed according to requirements.

--- src/main/java/com/example/masroofy/View/SetupView.java
+++ src/main/java/com/example/masroofy/View/SetupView.java
@@ -32,6 +32,5 @@ public class SetupView implements AbstractView {
         onSetupSubmitted(getAmountText(), getStartDateText(), getEndDateText());
     }
 
-    public void onSetupSubmitted(String amountText, String startText, String endText) {
-    }
+    public void onSetupSubmitted(String amountText, String startText, String endText) {}
 }
