bc2af229336d5d062622605d8b0a87804abd9707
Abdallah Mohamed <pedrito1172341@gmail.com>
2026-05-02 23:06:13 +0300

    Add SetupListener to Setup MVC, Change SetupView date into Date objects

--- src/main/java/com/example/masroofy/Controller/SetupController.java
+++ src/main/java/com/example/masroofy/Controller/SetupController.java
@@ -3,10 +3,25 @@ package com.example.masroofy.Controller;
 import com.example.masroofy.Model.*;
 import com.example.masroofy.View.*;
 import java.util.Date;
+import com.example.masroofy.Listener.SetupListener;
+public class SetupController implements AbstractController, SetupListener {
+    private Setup model;
+    private SetupView view;
 
-public class SetupController implements AbstractController {
+    public SetupController(Setup m, SetupView v) {
+        model = m;
+        view = v;
+        view.setEventListener(this);
+    }
+
     @Override
-    public void PrintView() {}
+    public void PrintView() {
+        view.printScreen();
+    }
 
+    @Override
+    public void onSetupSumbitted(double allowance, Date startDate, Date endDate) {
+
+    }
     public void validateSetup(int allowanceAmount, Date startDate, Date endDate) {}
 }
---
+++ src/main/java/com/example/masroofy/Listener/SetupListener.java
@@ -0,0 +1,7 @@
+package com.example.masroofy.Listener;
+
+import java.util.Date;
+
+public interface SetupListener {
+    public void onSetupSumbitted(double allowance, Date startDate, Date endDate);
+}
